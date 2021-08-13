2d825e9ed42975995adaa264bcd073884d3909d2
Meelenych <80649931+Meelenych@users.noreply.github.com>
2021-08-13 17:54:26 +0300

    js files added

---
+++ js/gallery-markup.js
@@ -0,0 +1,10 @@
+import galleryItems from "./pictures.js";
+console.log(galleryItems)
+
+const list = document.querySelector('.gallery')
+console.log(list)
+
+const lightbox = document.querySelector('.lightbox')
+console.log(lightbox)
+
+
